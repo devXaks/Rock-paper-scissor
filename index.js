9fc94f4b94e85d82e013df7e78d5dfd903312223
Akshat-AI <mailtoakshattripathi@gmail.com>
2024-07-13 11:33:52 +0530

    pushed final project

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ alert("This website is made of education purposes intended to learn development.
 let userScore = 0;
 let compScore = 0;
 
-const choices = document.querySelectorAll(".choice");
+
+
+const choices = document.querySelectorAll(".choice")
 
 const genCompChoice = () => {
     const option = ["rock","paper","scissor"]
@@ -10,15 +12,33 @@ const genCompChoice = () => {
     return option[randInx]
 }
 
-const drawGame = () => {
+const drawGame = (userChoice) => {
+    msg.innerText = `That's a draw. Both chose ${userChoice}`
+    display.style.backgroundColor ="blue"
     console.log("Draw")
 }
+const userScoreP = document.querySelector("#user-score")
+const compScoreP = document.querySelector("#comp-score")
+const msg = document.querySelector("#disp-txt")
+const display = document.querySelector("#dsp")
 
-const showWinner = (userWin) => {
+const showWinner = (userWin,userChoice,compChoice) => {
     if (userWin) {
+        userScore++
         console.log("you win!")
-    } else{
+        userScoreP.innerText = userScore
+        msg.innerText = `You won, ${userChoice} beats ${compChoice}`
+        display.style.backgroundColor = "green"
+        
+        } else{
+        compScore++
         console.log("you lose")
+        compScoreP.innerText = compScore
+        msg.innerText = `You loose, ${compChoice} beats ${userChoice}`
+        display.style.backgroundColor = "red"
+
+
+
     }
 }
 
@@ -30,7 +50,7 @@ const playGame = (userChoice) => {
 
     if(userChoice === compChoice) {
         //the game is draw
-        drawGame()
+        drawGame(userChoice)
     }else{
         let userWin = true;
         if(userChoice === "rock"){
@@ -42,7 +62,7 @@ const playGame = (userChoice) => {
         } else if (userChoice === "scissor"){
             userWin = compChoice === "rock" ? false :true;
         }
-        showWinner(userWin)
+        showWinner(userWin,userChoice,compChoice)
     }
 }
 choices.forEach((choice) => {
@@ -51,3 +71,4 @@ choices.forEach((choice) => {
         playGame(userChoice)
     })
 })
+
